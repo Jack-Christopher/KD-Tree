e3d00666e281d55d12b14d57f719bbd5476091e6
Jack <jhuaihuah@unsa.edu.pe>
2021-10-31 08:32:24 -0500

    Borders ADDED

--- KDTree.py
+++ KDTree.py
@@ -12,10 +12,18 @@ class Node:
         self.point = point
         self.left = None
         self.right = None
+        self.bottom_left = None
+        self.top_right = None
+
     def setLeft(self, node):
         self.left = node
     def setRight(self, node):
         self.right = node
+
+    def set_bottom_left(self, node):
+        self.bottom_left = node
+    def set_top_right(self, node):
+        self.top_right = node
 
 
 class KDTree:   
@@ -58,19 +66,57 @@ class KDTree:
         node.setRight(right)
 
         return node
+
+    def prepare_to_plot(self, bottom_left: Point, top_right: Point):
+        self.root.set_bottom_left(bottom_left)
+        self.root.set_top_right(top_right)
+        self.prepare_to_plot_rec(self.root, 0, self.root)
+
+    def prepare_to_plot_rec(self, node, depth, parent: Node):
+        if node is None:
+            return
+        if depth % 2 == 0:
+            if node.left is not None:
+                node.left.set_bottom_left(parent.bottom_left)
+                node.left.set_top_right(Point(parent.point.x, parent.top_right.y))
+            if node.right is not None:
+                node.right.set_bottom_left(Point(parent.point.x, parent.bottom_left.y))
+                node.right.set_top_right(parent.top_right)
+        else:
+            if node.left is not None:
+                node.left.set_bottom_left(parent.bottom_left)
+                node.left.set_top_right(Point(parent.top_right.x, parent.point.y))
+            if node.right is not None:
+                node.right.set_bottom_left(Point(parent.bottom_left.x, parent.point.y))
+                node.right.set_top_right(parent.top_right)
+        
+        self.prepare_to_plot_rec(node.left, depth + 1, node.left)
+        self.prepare_to_plot_rec(node.right, depth + 1, node.right)
+
     
     def print_tree(self):
+        self.prepare_to_plot(Point(0, 0), Point(10, 10))
         self.print_tree_rec(self.root, 0)
     
     def print_tree_rec(self, node, depth):
         if node is None:
             return
-        print(" " * 2*depth, node.point.x, node.point.y)
+        print(" " * 2*depth, node.point.x, node.point.y, end=" ")
+        if node.left is not None:
+            print(": ", node.bottom_left.x, node.bottom_left.y, "--> ", node.top_right.x, node.top_right.y)
+        print()
         self.print_tree_rec(node.left, depth + 1)
         self.print_tree_rec(node.right, depth + 1)
-        
-
-
-        
     
-        
+
+    def draw(self):
+        self.draw_rec(self.root, 0)#, x_values, y_values)
+        # plt.plot(x_values, y_values, 'ro')
+        plt.show()
+    
+    def draw_rec(self, node, depth):#, x_values, y_values):
+        if node is None:
+            return
+        if depth % 2 == 0:
+            plt.plot([node.point.x, node.top_right.x], [node.point.y, node.top_right.y], 'b')
+            
